2b8d9ab0cb19bdeaa572563f9970c37d80cb598f
warmijusti <justinaparma@gmail.com>
2025-10-22 15:29:32 -0300

    implement ParaglideJS \n

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,44 +1,46 @@
-import { allDefined } from "@awesome.me/webawesome/dist/webawesome.js";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./contexts/AuthContext";
-import "./styles/index.css";
+import { allDefined } from '@awesome.me/webawesome/dist/webawesome.js'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { AuthProvider } from './contexts/AuthContext'
+import { LanguageProvider } from './contexts/LanguageContext'
+import './styles/index.css'
 
 // Import Web Awesome components needed by hanko-auth web component
-import "@awesome.me/webawesome/dist/components/dropdown/dropdown.js";
-import "@awesome.me/webawesome/dist/components/dropdown-item/dropdown-item.js";
-import "@awesome.me/webawesome/dist/components/button/button.js";
-import "@awesome.me/webawesome/dist/components/icon/icon.js";
+import '@awesome.me/webawesome/dist/components/dropdown/dropdown.js'
+import '@awesome.me/webawesome/dist/components/dropdown-item/dropdown-item.js'
+import '@awesome.me/webawesome/dist/components/button/button.js'
+import '@awesome.me/webawesome/dist/components/icon/icon.js'
 
 // Import Hanko auth web component
-import "../auth-libs/web-component/dist/hanko-auth.esm.js";
+import '../auth-libs/web-component/dist/hanko-auth.esm.js'
 
 // Set Hanko URL for authentication
-window.HANKO_URL =
-  import.meta.env.VITE_HANKO_URL || "https://dev.login.hotosm.org";
+window.HANKO_URL = import.meta.env.VITE_HANKO_URL || 'https://dev.login.hotosm.org'
 
 // Ensure all WebAwesome components are loaded before rendering
-await allDefined();
+await allDefined()
 
 // add providers here
 function Root() {
   return (
     <StrictMode>
       <BrowserRouter>
-        <AuthProvider>
-          <AppContent />
-        </AuthProvider>
+        <LanguageProvider>
+          <AuthProvider>
+            <AppContent />
+          </AuthProvider>
+        </LanguageProvider>
       </BrowserRouter>
     </StrictMode>
-  );
+  )
 }
 
 function AppContent() {
-  return <App />;
+  return <App />
 }
 
-const rootElement = document.getElementById("root")!;
-const root = createRoot(rootElement);
-root.render(<Root />);
+const rootElement = document.getElementById('root')!
+const root = createRoot(rootElement)
+root.render(<Root />)
