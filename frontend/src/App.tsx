c56dd2540d169200c7ad848c93ae7eb4cfa8d6fa
warmijusti <justinaparma@gmail.com>
2025-11-03 17:00:00 -0300

    deps and smooth scroll config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { AppRoutes } from "./routes";
 
 function App() {
+  useEffect(() => {
+    // Initialize Lenis smooth scroll
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+      smoothTouch: false,
+    });
+
+    // Request animation frame loop
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    // Cleanup
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="container">
