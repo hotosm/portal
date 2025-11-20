c59ca2f9aaaec70a68fb363f7283ddbdfbf7ccb6
warmijusti <justinaparma@gmail.com>
2025-11-20 12:01:28 -0300

    fix responsive on homepage

--- frontend/src/components/techSuite/TechSuiteItem.tsx
+++ frontend/src/components/techSuite/TechSuiteItem.tsx
@@ -1,38 +1,37 @@
-import { motion } from "motion/react";
-
 interface ITechSuiteItemProps {
   title: string;
   description: string;
   icon: any;
+  link: string;
 }
 
-function TechSuiteItem({ title, description, icon }: ITechSuiteItemProps) {
-  console.log(icon);
+function TechSuiteItem({
+  title,
+  description,
+  icon,
+  link,
+}: ITechSuiteItemProps) {
   return (
-    <motion.div
-      className="group flex flex-col justify-center items-center text-center hover:cursor-pointer"
-      whileHover="hover"
-    >
-      <motion.img
-        src={icon}
-        alt="Product vector icon"
-        style={{
-          height: "100px",
-          width: "100px",
-          margin: "0 auto",
-        }}
-        variants={{
-          hover: { scale: 1.1 },
-        }}
-        transition={{ type: "spring", stiffness: 250, damping: 15 }}
-      />
-      <p className="m-xs">
-        <span className="text-white text-lg uppercase px-sm bg-hot-red-600 group-hover:bg-hot-red-400 transition-opacity rounded-sm pb-[2px]">
-          {title}
-        </span>
-      </p>
-      <p className="max-w-[300px]">{description}</p>
-    </motion.div>
+    <div className="group flex flex-col justify-center items-center text-center hover:cursor-pointer">
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:no-underline"
+      >
+        <img
+          src={icon}
+          alt="Product vector icon"
+          className="h-[100px] w-[100px] mx-auto transition-transform duration-300 ease-in-out group-hover:scale-110"
+        />
+        <p className="m-xs">
+          <span className="text-white text-lg uppercase px-sm bg-hot-red-600 group-hover:bg-hot-red-400 transition-all duration-300 ease-in-out rounded-sm pb-[2px] inline-block group-hover:scale-105 group-hover:translate-y-1 leading-none">
+            {title}
+          </span>
+        </p>
+        <p className="max-w-[300px]">{description}</p>
+      </a>
+    </div>
   );
 }
 
