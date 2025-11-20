c215c084fcd8198f0370cc50f81dd20c8cfaf4ca
warmijusti <52356603+warmijusti@users.noreply.github.com>
2025-11-20 10:55:37 -0500

    Revert "Feature/tool pages"

--- frontend/src/components/shared/Tag.tsx
+++
@@ -1,9 +0,0 @@
-import WaTag from "@awesome.me/webawesome/dist/react/tag/index.js";
-
-export interface TagProps extends React.ComponentProps<typeof WaTag> {}
-
-function Tag(props: TagProps) {
-  return <WaTag {...props} />;
-}
-
-export default Tag;
