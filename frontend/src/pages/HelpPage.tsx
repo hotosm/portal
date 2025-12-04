e5fcfea9ff4fc19d7972d975ef1bf9ca999e4b4e
warmijusti <justinaparma@gmail.com>
2025-12-04 14:42:38 -0300

    help page

---
+++ frontend/src/pages/HelpPage.tsx
@@ -0,0 +1,13 @@
+import PageWrapper from "../components/shared/PageWrapper";
+
+function HelpPage() {
+  return (
+    <PageWrapper>
+      <div className="space-y-xl">
+        <h2>Help</h2>
+      </div>
+    </PageWrapper>
+  );
+}
+
+export default HelpPage;
