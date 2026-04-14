9259696e716f51022aed11c5ba388833b248fcf3
warmijusti <justinaparma@gmail.com>
2026-04-14 11:33:51 -0500

    navigation translations

--- frontend/src/constants/menu.ts
+++ frontend/src/constants/menu.ts
@@ -1,7 +1,9 @@
+import * as m from "../paraglide/messages";
+
 // Menu items for HOT Portal navigation
 export interface MenuItem {
   id: string;
-  label: string;
+  label: () => string;
   href: string;
   description?: string;
   external?: boolean;
@@ -12,31 +14,31 @@ export interface MenuItem {
 export const MAIN_MENU_ITEMS: MenuItem[] = [
   {
     id: "imagery",
-    label: "Imagery",
+    label: m.section_imagery,
     href: "/imagery",
     requiresAuth: false,
   },
   {
     id: "mapping",
-    label: "Mapping",
+    label: m.section_mapping,
     href: "/mapping",
     requiresAuth: false,
   },
   {
     id: "field",
-    label: "Field",
+    label: m.section_field,
     href: "/field",
     requiresAuth: false,
   },
   {
     id: "data",
-    label: "Data",
+    label: m.section_data,
     href: "/data",
     requiresAuth: false,
   },
   {
     id: "learn",
-    label: "Learn",
+    label: () => "Learn",
     href: "/learn",
     requiresAuth: false,
     icon: "patch-check",
