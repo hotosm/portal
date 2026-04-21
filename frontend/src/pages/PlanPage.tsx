fc5f6f3a3748dff404cfbd9984a60a9424742370
warmijusti <justinaparma@gmail.com>
2026-04-21 12:56:03 -0500

    not found and plan page

---
+++ frontend/src/pages/PlanPage.tsx
@@ -0,0 +1,13 @@
+import PageWrapper from "../components/shared/PageWrapper";
+
+function PlanPage() {
+  return (
+    <PageWrapper>
+      <div className="space-y-xl">
+        <h2>Comming soon</h2>
+      </div>
+    </PageWrapper>
+  );
+}
+
+export default PlanPage;
